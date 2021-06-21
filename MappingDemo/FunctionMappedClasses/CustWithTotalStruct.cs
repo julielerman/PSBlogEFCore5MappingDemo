3111da7f36953e2f4b0280c03ed95051f455fbdb
Julie Lerman <julielerman@users.noreply.github.com>
2021-06-21 14:45:23 -0400

    both functions working when mapped

---
+++ MappingDemo/FunctionMappedClasses/CustWithTotalStruct.cs
@@ -0,0 +1,13 @@
+﻿namespace MappingDemo
+{
+    public struct CustWithTotalStruct
+    {
+        public CustWithTotalStruct(string name, int total)
+        {
+            Name = name;
+            TotalSpent = total;
+        }
+        public string Name { get; }
+        public int TotalSpent { get; }
+    }
+}
